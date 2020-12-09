715f37707b683aea92ea06fb5dcb81779c3631f5
Hans Tananda <hanstananda@gmail.com>
2020-12-09 23:15:19 +0800

    added colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -5,6 +5,14 @@ const colors = {
   lightGrayPurple: '#f7f7fb',
   pink: '#ff3d69',
   gray: '#797777',
+
+  white: '#FFFFFF',
+  orange: '#FE5F00',
+  lightGreen: '#00CC77',
+  darkGreen: '#00884F',
+  black: '#050505',
+  lightGray: '#EBE9E9',
+  blue: '#054A91',
 }
 
 export default colors
