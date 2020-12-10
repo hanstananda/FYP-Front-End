bf816863a6027f0bd5818fbf617755986f09aa50
Hans Tananda <hanstananda@gmail.com>
2020-12-10 12:23:20 +0800

    update home page

--- src/scenes/home/Home.js
+++ src/scenes/home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  StyleSheet, Text, View, StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TouchableOpacity,
 } from 'react-native'
 import Button from 'components/Button'
-import { colors } from 'theme'
+import { colors, images } from 'theme'
+import Svg from 'components/Svg'
+import { SvgUri, SvgXml } from 'react-native-svg'
 
 const styles = StyleSheet.create({
   root: {
@@ -14,24 +21,90 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: colors.lightGrayPurple,
   },
+  flexBox: {
+    flexDirection: 'row',
+    padding: 5,
+    minWidth: 300,
+    height: 70,
+  },
+  leftIconStyle: {
+    flex: 0.2,
+    backgroundColor: 'blue',
+  },
+  midText: {
+    // flex: 0.7,
+    backgroundColor: colors.blue,
+    alignItems: 'center',
+  },
+  rightIconStyle: {
+    flex: 0.1,
+    backgroundColor: 'red',
+  },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 20,
+    color: colors.white,
+    marginBottom: 5,
+  },
+  desc: {
+    fontSize: 10,
+    color: colors.white,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 50,
+  },
+  button: {
+    color: colors.white,
+    backgroundColor: colors.orange,
+    alignItems: 'center',
+    minWidth: 200,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.orange,
+    marginBottom: 50,
   },
 })
 
 const Home = ({ navigation }) => (
   <View style={styles.root}>
     <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Home</Text>
-    <Button
-      title="Go to Details"
-      color="white"
-      backgroundColor={colors.lightPurple}
+    <Image style={styles.logo} source={images.logo_sm} />
+    {/* <Text style={styles.title}>Home</Text> */}
+    <TouchableOpacity
+      style={styles.button}
       onPress={() => {
         navigation.navigate('Details', { from: 'Home' })
       }}
-    />
+    >
+      <View style={styles.flexBox}>
+        <View style={styles.leftIconStyle}>
+          <SvgUri
+            width="100%"
+            height="100%"
+            uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+          />
+        </View>
+        <View style={styles.midText}>
+          <Text style={styles.title}>Identify Snakes</Text>
+          <Text style={styles.desc}>Came across a snake?</Text>
+          <Text style={styles.desc}>
+            Select its image and classify what it was
+          </Text>
+        </View>
+        <View style={styles.rightIconStyle}>
+          <Text>Test</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+    {/* <Button */}
+    {/*  title="Identify Snakes" */}
+    {/*  color="white" */}
+    {/*  backgroundColor={colors.orange} */}
+    {/*  onPress={() => { */}
+    {/*    navigation.navigate('Details', { from: 'Home' }) */}
+    {/*  }} */}
+    {/* /> */}
   </View>
 )
 
