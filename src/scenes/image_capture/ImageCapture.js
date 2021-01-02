5ab563c4c197f3927ca0b7da4cf78ac8c860666f
Hans Tananda <hanstananda@gmail.com>
2021-01-02 17:56:08 +0800

    add buttons, refactor styling in image capture page

--- src/scenes/image_capture/ImageCapture.js
+++ src/scenes/image_capture/ImageCapture.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { buttonStyles, colors } from '../../theme'
 
 const styles = StyleSheet.create({
@@ -17,34 +18,49 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: colors.lightGrayPurple,
   },
+  topButton: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
   flexBox: {
+    flex: 1,
     flexDirection: 'row',
     padding: 5,
+    maxWidth: 300,
   },
   leftIconStyle: {
     flex: 0.15,
     alignItems: 'center', // Centered horizontally
     justifyContent: 'center', // Centered vertically
   },
-  iconStyle: {
-    height: 30,
-    width: 30,
-  },
   midText: {
-    flex: 0.75,
-    alignItems: 'center',
+    flex: 0.85,
+    paddingLeft: 20,
+    alignItems: 'flex-start',
+    justifyContent: 'center', // Centered vertically
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 })
 
 const ImageCapture: React.FC = ({ navigation }) => {
   const [image, setImage] = useState(null)
-  const btnImageCaptureStyle = [
+  const btnImageFromFileStyle = [
     buttonStyles.defaultButtonStyle,
     buttonStyles.altButtonStyle,
+    styles.topButton,
   ]
+  const btnImageCaptureStyle = [
+    buttonStyles.defaultButtonStyle,
+    buttonStyles.mainButtonStyle,
+    styles.topButton,
+  ]
+  const folderTextStyle = [styles.text, { color: colors.darkGreen }]
+  const cameraTextSTyle = [styles.text, { color: colors.white }]
 
   useEffect(() => {
     (async () => {
@@ -76,13 +92,30 @@ const ImageCapture: React.FC = ({ navigation }) => {
 
   return (
     <View style={styles.root}>
-      <TouchableOpacity style={btnImageCaptureStyle} onPress={pickImage}>
-        <View>
-          <Text>Pick an image from camera roll</Text>
+      <TouchableOpacity style={btnImageFromFileStyle} onPress={pickImage}>
+        <View style={styles.flexBox}>
+          <View style={styles.leftIconStyle}>
+            <FontAwesomeIcon icon="folder" color={colors.darkGreen} size={32} />
+          </View>
+
+          <View style={styles.midText}>
+            <Text style={folderTextStyle}>Select from Files</Text>
+          </View>
         </View>
       </TouchableOpacity>
+      <Image source={{ uri: image }} style={{ width: 299, height: 299 }} />
       {image && (
-        <Image source={{ uri: image }} style={{ width: 224, height: 224 }} />
+        <TouchableOpacity style={btnImageCaptureStyle}>
+          <View style={styles.flexBox}>
+            <View style={styles.leftIconStyle}>
+              <FontAwesomeIcon icon="camera" color={colors.white} size={32} />
+            </View>
+
+            <View style={styles.midText}>
+              <Text style={cameraTextSTyle}>Classify Snake!</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
       )}
     </View>
   )
