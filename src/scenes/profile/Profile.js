b787ab17256a2a0ace2d16cf1c2975a330917b11
Hans Tananda <hanstananda@gmail.com>
2021-01-13 15:42:33 +0800

    add fontawesome regular icon, added placeholder map info, implement profile (wip)

--- src/scenes/profile/Profile.js
+++ src/scenes/profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  StyleSheet, Text, View, StatusBar,
+  StyleSheet, Text, View, StatusBar, TextInput,
 } from 'react-native'
 import Button from 'components/Button'
 import { colors } from 'theme'
@@ -11,29 +11,45 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     backgroundColor: colors.lightGrayPurple,
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
   },
+  formInput: {
+    height: 40,
+    minWidth: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 25,
+  },
 })
 
-const Profile = ({ navigation }) => (
-  <View style={styles.root}>
-    <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Profile</Text>
-    <Button
-      title="Go to Details"
-      color="white"
-      backgroundColor={colors.lightPurple}
-      onPress={() => {
-        navigation.navigate('SnakeList', { from: 'Profile' })
-      }}
-    />
-  </View>
-)
+const Profile = ({ navigation }) => {
+  const [value, onChangeText] = React.useState('Useless Placeholder')
+
+  return (
+    <View style={styles.root}>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Profile</Text>
+      <TextInput
+        style={styles.formInput}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+      />
+      {/* <Button */}
+      {/*  title="Go to Details" */}
+      {/*  color="white" */}
+      {/*  backgroundColor={colors.lightPurple} */}
+      {/*  onPress={() => { */}
+      {/*    navigation.navigate('SnakeList', { from: 'Profile' }) */}
+      {/*  }} */}
+      {/* /> */}
+    </View>
+  )
+}
 
 Profile.propTypes = {
   navigation: PropTypes.shape({ navigate: PropTypes.func }),
