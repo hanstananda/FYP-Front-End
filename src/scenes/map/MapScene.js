1951e6c7f976dd7f58a7ef02ebdd2f6b289f20d4
Hans Tananda <hanstananda@gmail.com>
2021-03-05 01:41:08 +0800

    added testing of map

--- src/scenes/map/MapScene.js
+++ src/scenes/map/MapScene.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import {
-  StyleSheet, Text, View, StatusBar,
+  StyleSheet, Text, View, StatusBar, Dimensions,
 } from 'react-native'
 import { colors } from 'theme'
+import MapView, { Marker } from 'react-native-maps'
 import getSnakeInfoList from '../../services/SnakeInfo/getSnakeInfoList'
 import { logout } from '../../modules/app.module'
 
@@ -19,6 +20,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 })
 
 const MapScene = ({ navigation }) => {
@@ -28,7 +33,23 @@ const MapScene = ({ navigation }) => {
   return (
     <View style={styles.root}>
       <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>MapView - Pending Request Approval</Text>
+      <MapView
+        initialRegion={{
+          latitude: 1.29,
+          longitude: 103.85,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        style={styles.map}
+      >
+        <Marker
+          coordinate={{ latitude: 1.35, longitude: 103.68 }}
+          title="Asian Vine Snake"
+          description="Not that dangerous"
+        />
+      </MapView>
+
+      {/* <Text style={styles.title}>MapView - Pending Request Approval</Text> */}
     </View>
   )
 }
